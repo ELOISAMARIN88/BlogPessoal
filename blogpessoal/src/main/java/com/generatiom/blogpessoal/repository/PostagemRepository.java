1871be7dd43f1a7da1ebde06e09abfebb96066fc
Eloisa Marin <eloisamarin7@gmail.com>
2024-01-11 16:15:57 -0300

    Postagem.java, PostagemRepository.java e PostagemController.java

---
+++ blogpessoal/src/main/java/com/generatiom/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,10 @@
+package com.generatiom.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generatiom.blogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+}
+
